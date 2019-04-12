2946fb49afc00b4431e4b137c50235fd5e26d96a
John Scott <john@jjrscott.com>
2019-04-12 20:12:32 +0100

    Initial Commit

---
+++ Lame/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Lame
+//
+//  Created by John Scott on 12/04/2019.
+//  Copyright © 2019 John Scott. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
